0f28e0700f648c41deaa44ee6cf9d3bee1a08d53
ann0va <anna.zhosan7@gmail.com>
2023-09-01 15:09:42 +0200

    improve chat message in chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -15,7 +15,15 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
-START, AFTER_YES, CATEGORY_SELECTED, PLEASURE_MANN, PLEASURE_WOMAN, MAIN_MENU = range(6)
+(START,
+ AFTER_YES,
+ CATEGORY_SELECTED,
+ PLEASURE_GENDER,
+ ERECTION,
+ ERECTION_SELECTED,
+ INCREASE_SIZE,
+ ANAL_PLEASURE_SELECTED,
+ MAIN_MENU) = range(9)
 
 
 async def start(update: Update, _context) -> int:
@@ -32,9 +40,10 @@ async def start(update: Update, _context) -> int:
     return START
 
 
-async def ask_interests(update: Update, context) -> int:
+
+async def ask_interests(update: Update, _context) -> int:
+
     user_response = update.message.text
-    context.user_data['interests'] = user_response  # Store user response in context
 
     if user_response == "Так":
         reply_keyboard = [
@@ -50,7 +59,9 @@ async def ask_interests(update: Update, context) -> int:
         )
         return AFTER_YES
     elif user_response == "Ні":
-        await update.message.reply_text("Вам ще зарано сюди. Дякуємо за увагу, та чекаємо коли буде 18+!")
+        await update.message.reply_text(
+            "Вам ще зарано сюди. Дякуємо за увагу, та чекаємо коли буде 18+!"
+        )
         return ConversationHandler.END
 
 
@@ -62,27 +73,34 @@ async def category_selected(update: Update, context) -> int:
             ["Чоловік", "Жінка"],
             ["Вихід"],
         ]
-        await update.message.reply_text("Ви обрали 'Відчуйте насолоду'. Для кого ти шукаєш іграшку цього разу?",
+        await update.message.reply_text(
+            "Ви обрали 'Відчуйте насолоду'. Для кого ти шукаєш іграшку цього разу?",
             reply_markup=ReplyKeyboardMarkup(
                 reply_keyboard, one_time_keyboard=True, resize_keyboard=True
             ),
         )
         return CATEGORY_SELECTED
     elif user_response == "Розкрий свою уяву":
-        await update.message.reply_text("Ви обрали 'Розкрий свою уяву'. Поки що ця категорія знаходиться в розробці.")
+        await update.message.reply_text(
+            "Ви обрали 'Розкрий свою уяву'. Поки що ця категорія знаходиться в розробці."
+        )
         return ConversationHandler.END  # End the conversation
     elif user_response == "Подбати про здоров'я":
-        await update.message.reply_text("Ви обрали 'Подбати про здоров\'я'. Поки що ця категорія знаходиться в розробці.")
+        await update.message.reply_text(
+            "Ви обрали 'Подбати про здоров\'я'. Поки що ця категорія знаходиться в розробці."
+        )
         return ConversationHandler.END  # End the conversation
     elif user_response == "Придбати подарунки та сертифікати":
-        await update.message.reply_text("Ви обрали 'Придбати подарунки та сертифікати'. Поки що ця категорія знаходиться в розробці.")
+        await update.message.reply_text(
+            "Ви обрали 'Придбати подарунки та сертифікати'. Поки що ця категорія знаходиться в розробці."
+        )
         return ConversationHandler.END  # End the conversation
     elif user_response == "Вихід":
-        await update.message.reply_text("Дякую, що обрали нас!")
+        await cancel(update, context)
         return ConversationHandler.END
 
 
-async def pleasure_mann(update: Update, _context) -> int:
+async def pleasure_gender(update: Update, _context) -> int:
     user_response = update.message.text
 
     if user_response == "Чоловік":
@@ -95,13 +113,7 @@ async def pleasure_mann(update: Update, _context) -> int:
             reply_markup=ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True),
             disable_web_page_preview=True,
         )
-        return PLEASURE_MANN
-
-
-async def pleasure_woman(update: Update, _context) -> int:
-    user_response = update.message.text
-
-    if user_response == 'Жінка':
+    elif user_response == "Жінка":
         reply_keyboard = [
             ["Підвищення інтенсивності оргазмів", "Приємні відчуття", "Подвійна насолода (кліторально-вагінальна зона)",
              "Анальне задоволення"],
@@ -112,7 +124,7 @@ async def pleasure_woman(update: Update, _context) -> int:
             reply_markup=ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True),
             disable_web_page_preview=True,
         )
-        return PLEASURE_WOMAN
+        return PLEASURE_GENDER
 
 
 async def main_menu(update: Update, _context) -> int:
@@ -126,15 +138,19 @@ async def main_menu(update: Update, _context) -> int:
         ]
         await update.message.reply_text(
             f"Вітаю, {update.message.from_user.first_name}. Оберіть, будь ласка, опцію:",
-            reply_markup=ReplyKeyboardMarkup(
-                reply_keyboard, one_time_keyboard=True, resize_keyboard=True
-            ),
+            reply_markup=ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True),
+            disable_web_page_preview=True,
         )
         return AFTER_YES
+    elif user_response == "Вихід":
+        await cancel(update, _context)
+        return ConversationHandler.END
 
+    return MAIN_MENU
 
 async def cancel(update: Update, _context) -> int:
     user = update.message.from_user
+    user_response = update.message.text
 
     if user_response == "Вихід":
         logger.info("User %s canceled the conversation.", user.first_name)
@@ -144,8 +160,113 @@ async def cancel(update: Update, _context) -> int:
         )
         return ConversationHandler.END
 
+async def erection(update: Update, _context) -> int:
+    user_response = update.message.text
+    if user_response == 'Підвищення ерекції':
+        reply_keyboard = [
+            ["Насадки на член", "Ерекційні кільця"],
+            ["Назад", "Головне меню"],
+        ]
+        await update.message.reply_text(
+            "О, це вже серйозно!"
+            "\nДля більш стійкої й крутезної ерекції ми маємо декілька варіантів: насадки, ерекційні кільця та ласо."
+            "\n Що Ви бажаєте?",
+            reply_markup=ReplyKeyboardMarkup(
+                reply_keyboard, one_time_keyboard=True, resize_keyboard=True
+            ),
+        )
+        return ERECTION
 
+
+async def erection_selected(update: Update, _context) -> int:
+    user_response = update.message.text
+    if user_response == "Насадки на член":
+        await update.message.reply_text(
+            "Насадки на член для подовження – це як підвищення рівня на ваших ігрових ресурсах."
+            "\nЗверніть увагу, що насадки можна використовувати"
+            " з лубрикантами лише на водній основі та антисептиками без спирту."
+            "\nОберіть та дізнайтесь більше за посиланням."
+            "\nОсь список рекомендованих товарів: "
+        )
+    elif user_response == "Ерекційні кільця":
+        reply_keyboard = [["Назад", "Головне меню"]]
+        await update.message.reply_text(
+            "Доктор Крутизна вітає на борту! Давайте розберемося з ерекційними кільцями."
+            "\nЗверніть увагу, що ерекційні кільця можна використовувати з лубрикантами "
+            "лише на водній основі та антисептиками без спирту."
+            "\nОберіть та дізнайтесь більше за посиланням."
+            "\nОсь список рекомендованих товарів: ",
+            reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True),
+        )
+
+    return ERECTION_SELECTED
+async def increase_size(update: Update, _context) -> int:
+
+    user_response = update.message.text
+    if user_response == 'Збільшення розміру пеніса':
+        reply_keyboard = [
+            ["Насадки на член", "Ерекційні кільця"],
+            ["Назад", "Головне меню"],
+        ]
+        await update.message.reply_text(
+            "О, це вже серйозно!"
+            "\nДля більш стійкої й крутезної ерекції ми маємо декілька варіантів: насадки, ерекційні кільця та ласо."
+            "\n Що Ви бажаєте?",
+            reply_markup=ReplyKeyboardMarkup(
+                reply_keyboard, one_time_keyboard=True, resize_keyboard=True
+            ),
+        )
+    return INCREASE_SIZE
+
+async def anal_pleasure(update: Update, _context) -> int:
+
+    user_response = update.message.text
+    if user_response == 'Анальне задоволення':
+        reply_keyboard = [
+            ["Анальні кульки, ланцюжки, намиста", "Анальні вібратори", "Анальні пробки",
+             "Насадки для подвійного проникнення"],
+            ["Назад", "Головне меню"],
+        ]
+        await update.message.reply_text(
+            "А тут ми маємо 'Ворота до Веселощів!'"
+            "\nШукаєш анальне задоволення, обери що тебе зараз цікавить?",
+            reply_markup=ReplyKeyboardMarkup(
+                reply_keyboard, one_time_keyboard=True, resize_keyboard=True
+            ),
+        )
+        return ANAL_PLEASURE_SELECTED
+
+async def anal_pleasure_selected(update: Update, _context) -> int:
+
+    user_response = update.message.text
+    if user_response == "Анальні кульки, ланцюжки, намиста":
+        await update.message.reply_text(
+            "Онови свій інтимний 'арсенал'!"
+            "\nАнальні кульки, ланцюжки та намиста – ці аксесуари виявляться "
+            "кращими друзями під час вашої веселої ігрової вечірки!"
+        )
+    elif user_response == "Анальні вібратори":
+        await update.message.reply_text(
+            "Онови свій інтимний 'арсенал'!"
+            "\nЯкщо ви налаштовані на вібрації на абсолютно новому рівні, "
+            "то анальні вібратори вже чекають, щоб внести справжній 'віу-віу' ефект у ваше життя!"
+        )
+    elif user_response == "Анальні пробки":
+        await update.message.reply_text(
+            "Онови свій інтимний 'арсенал'!"
+            "\nАнальні пробки – ваш надійний співучасник у пошуках наповненості "
+            "та веселощів для постійної або тимчасової стимуляції анусу."
+        )
+    elif user_response == "Насадки для подвійного проникнення":
+        await update.message.reply_text(
+            "Онови свій інтимний 'арсенал'!"
+            "\nЯкщо ви налаштовані на новий рівень стимуляції, "
+            "то насадки для подвійного проникнення готові додати цілу гаму відчуттів "
+            "у ваші інтимні моменти."
+        )
+    return ANAL_PLEASURE_SELECTED
 def main() -> None:
+
     application = Application.builder().token("6462101482:AAHktf763kunhUdcU-sH8_8HUuJU7SdqK4k").build()
 
     conv_handler = ConversationHandler(
@@ -154,21 +275,26 @@ def main() -> None:
             START: [MessageHandler(filters.Text() & ~filters.COMMAND, ask_interests)],
             AFTER_YES: [
                 MessageHandler(filters.Text() & ~filters.COMMAND, category_selected),
-                MessageHandler(filters.Text() & ~filters.COMMAND, pleasure_mann),
-                MessageHandler(filters.Text() & ~filters.COMMAND, pleasure_woman),
+                MessageHandler(filters.Text() & ~filters.COMMAND, pleasure_gender),
+                MessageHandler(filters.Text() & ~filters.COMMAND, erection),
                 MessageHandler(filters.Text() & ~filters.COMMAND, main_menu),
             ],
             CATEGORY_SELECTED: [
-                MessageHandler(filters.Text() & ~filters.COMMAND, pleasure_mann),
-                MessageHandler(filters.Text() & ~filters.COMMAND, pleasure_woman),
+                MessageHandler(filters.Text() & ~filters.COMMAND, pleasure_gender),
                 MessageHandler(filters.Text() & ~filters.COMMAND, main_menu),
             ],
-            PLEASURE_MANN: [
+            PLEASURE_GENDER: [
                 MessageHandler(filters.Text() & ~filters.COMMAND, main_menu),
             ],
-            PLEASURE_WOMAN: [
-                MessageHandler(filters.Text() & ~filters.COMMAND, main_menu),
+            ERECTION: [
+                MessageHandler(filters.Text() & ~filters.COMMAND, erection_selected),
             ],
+            INCREASE_SIZE: [
+                MessageHandler(filters.Text() & ~filters.COMMAND, increase_size),
+            ],
+            ANAL_PLEASURE_SELECTED: [
+                MessageHandler(filters.Text() & ~filters.COMMAND, anal_pleasure_selected),
+            ]
         },
         fallbacks=[CommandHandler("cancel", cancel)],
     )
